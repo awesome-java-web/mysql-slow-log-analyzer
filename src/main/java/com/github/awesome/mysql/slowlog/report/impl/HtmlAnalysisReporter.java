2540650a3ab3dced08567b808ac678d0b98051f2
codeboyzhou <imzhouchen@gmail.com>
2025-02-25 02:00:14 +0800

    refactor(report): Refactor html analysis report generation logic using Thymeleaf engine

--- src/main/java/com/github/awesome/mysql/slowlog/report/impl/HtmlAnalysisReporter.java
+++ src/main/java/com/github/awesome/mysql/slowlog/report/impl/HtmlAnalysisReporter.java
@@ -1,70 +1,62 @@
 package com.github.awesome.mysql.slowlog.report.impl;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.awesome.mysql.slowlog.analyzer.model.AnalysisResult;
 import com.github.awesome.mysql.slowlog.config.Config;
 import com.github.awesome.mysql.slowlog.enums.StringSymbols;
 import com.github.awesome.mysql.slowlog.report.AnalysisReporter;
 import lombok.AllArgsConstructor;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static java.util.stream.Collectors.joining;
-
 @AllArgsConstructor
 public class HtmlAnalysisReporter implements AnalysisReporter {
 
-    private static final String HTML_TEMPLATE_NAME_ZH_CN = "mysql-slow-log-analysis-report-template-zh_CN.html";
+    private static final String HTML_TEMPLATE_NAME = "mysql-slow-log-analysis-report-template.html";
 
-    private static final String HTML_TEMPLATE_NAME_EN_US = "mysql-slow-log-analysis-report-template-en_US.html";
+    private static final String HTML_TEMPLATE_SUFFIX = ".html";
 
     private final Config config;
 
     @Override
     public void report(AnalysisResult result) {
-        renderHtml(result, HTML_TEMPLATE_NAME_ZH_CN);
-        renderHtml(result, HTML_TEMPLATE_NAME_EN_US);
-    }
-
-    private void renderHtml(AnalysisResult result, String reportTemplateName) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        try (InputStream inputStream = classLoader.getResourceAsStream(reportTemplateName)) {
-            if (inputStream == null) {
-                return;
-            }
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            final String template = reader.lines().collect(joining(StringSymbols.SPACE.getSymbol()));
-            ObjectMapper mapper = new ObjectMapper(new JsonFactory());
-            final String html = template
-                .replace("{{totalSlowQueries}}", String.valueOf(result.getTotalSlowQueries()))
-                .replace("{{averageQueryTimeMillis}}", result.getAverageQueryTimeMillis().toPlainString())
-                .replace("{{slowestQueryTimeMillis}}", result.getSlowestQuery().getQueryTimeMillis().toPlainString())
-                .replace("{{averageLockTimeMillis}}", result.getAverageLockTimeMillis().toPlainString())
-                .replace("{{longestLockTimeMillis}}", result.getLongestLockTimeQuery().getLockTimeMillis().toPlainString())
-                .replace("{{maxRowsSent}}", String.valueOf(result.getMaxRowsSentQuery().getRowsSent()))
-                .replace("{{maxRowsExamined}}", String.valueOf(result.getMaxRowsExaminedQuery().getRowsExamined()))
-                .replace("{{minRowsEfficiency}}", result.getMinRowsEfficiencyQuery()
-                    .getRowsEfficiency()
-                    .movePointRight(2)
-                    .setScale(2, RoundingMode.HALF_UP)
-                    .toPlainString()
-                )
-                .replace("{{topSlowQueries}}", String.valueOf(config.getTopSlowQueries()))
-                .replace("const topSlowQueries = []", "const topSlowQueries = " + mapper.writeValueAsString(result.getTopSlowQueries()));
-            final String reportName = reportTemplateName.replace("-template", StringSymbols.EMPTY.getSymbol());
-            Files.write(Paths.get(config.getAnalysisReportPath(), reportName), html.getBytes(StandardCharsets.UTF_8));
+        try {
+            renderHtml(result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void renderHtml(AnalysisResult result) throws IOException {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        templateResolver.setSuffix(HTML_TEMPLATE_SUFFIX);
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("totalSlowQueries", result.getTotalSlowQueries());
+        context.setVariable("averageQueryTimeMillis", result.getAverageQueryTimeMillis().toPlainString());
+        context.setVariable("slowestQueryTimeMillis", result.getSlowestQuery().getQueryTimeMillis().toPlainString());
+        context.setVariable("averageLockTimeMillis", result.getAverageLockTimeMillis().toPlainString());
+        context.setVariable("longestLockTimeMillis", result.getLongestLockTimeQuery().getLockTimeMillis().toPlainString());
+        context.setVariable("maxRowsSent", result.getMaxRowsSentQuery().getRowsSent());
+        context.setVariable("maxRowsExamined", result.getMaxRowsExaminedQuery().getRowsExamined());
+        context.setVariable("minRowsEfficiency", result.getMinRowsEfficiencyQuery()
+            .getRowsEfficiency()
+            .movePointRight(2)
+            .toPlainString()
+        );
+        context.setVariable("topSlowQueries", result.getTopSlowQueries());
+
+        final String html = templateEngine.process(HtmlAnalysisReporter.HTML_TEMPLATE_NAME, context);
+        final String reportName = HtmlAnalysisReporter.HTML_TEMPLATE_NAME.replace("-template", StringSymbols.EMPTY.getSymbol());
+        Files.write(Paths.get(config.getAnalysisReportPath(), reportName), html.getBytes(StandardCharsets.UTF_8));
     }
 
 }
