4f4b1b20c5c5c35fd9f43cd74d9e7a2e4815b611
codeboyzhou <imzhouchen@gmail.com>
2025-02-25 20:45:35 +0800

    feat(report): Add i18n support for html analysis report

--- src/main/java/com/github/awesome/mysql/slowlog/report/impl/HtmlAnalysisReporter.java
+++ src/main/java/com/github/awesome/mysql/slowlog/report/impl/HtmlAnalysisReporter.java
@@ -2,7 +2,6 @@ package com.github.awesome.mysql.slowlog.report.impl;
 
 import com.github.awesome.mysql.slowlog.analyzer.model.AnalysisResult;
 import com.github.awesome.mysql.slowlog.config.Config;
-import com.github.awesome.mysql.slowlog.enums.StringSymbols;
 import com.github.awesome.mysql.slowlog.report.AnalysisReporter;
 import lombok.AllArgsConstructor;
 import org.thymeleaf.TemplateEngine;
@@ -13,6 +12,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
 
 @AllArgsConstructor
 public class HtmlAnalysisReporter implements AnalysisReporter {
@@ -21,18 +23,23 @@ public class HtmlAnalysisReporter implements AnalysisReporter {
 
     private static final String HTML_TEMPLATE_SUFFIX = ".html";
 
+    private static final String I18N_BASE_NAME = "html_analysis_report_i18n";
+
     private final Config config;
 
     @Override
     public void report(AnalysisResult result) {
+        ResourceBundle i18nChinese = ResourceBundle.getBundle(I18N_BASE_NAME, Locale.CHINA);
+        ResourceBundle i18nEnglish = ResourceBundle.getBundle(I18N_BASE_NAME, Locale.US);
         try {
-            renderHtml(result);
+            renderHtml(result, i18nChinese);
+            renderHtml(result, i18nEnglish);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void renderHtml(AnalysisResult result) throws IOException {
+    private void renderHtml(AnalysisResult result, ResourceBundle i18n) throws IOException {
         TemplateEngine templateEngine = new TemplateEngine();
         ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
@@ -40,6 +47,7 @@ public class HtmlAnalysisReporter implements AnalysisReporter {
         templateEngine.setTemplateResolver(templateResolver);
 
         Context context = new Context();
+
         context.setVariable("totalSlowQueries", result.getTotalSlowQueries());
         context.setVariable("averageQueryTimeMillis", result.getAverageQueryTimeMillis().toPlainString());
         context.setVariable("slowestQueryTimeMillis", result.getSlowestQuery().getQueryTimeMillis().toPlainString());
@@ -54,8 +62,14 @@ public class HtmlAnalysisReporter implements AnalysisReporter {
         );
         context.setVariable("topSlowQueries", result.getTopSlowQueries());
 
+        Set<String> i18nKeys = i18n.keySet();
+        for (String key : i18nKeys) {
+            context.setVariable(key, i18n.getString(key));
+        }
+
         final String html = templateEngine.process(HtmlAnalysisReporter.HTML_TEMPLATE_NAME, context);
-        final String reportName = HtmlAnalysisReporter.HTML_TEMPLATE_NAME.replace("-template", StringSymbols.EMPTY.getSymbol());
+        final String i18nSuffix = String.format("%s_%s", i18n.getLocale().getLanguage(), i18n.getLocale().getCountry());
+        final String reportName = HtmlAnalysisReporter.HTML_TEMPLATE_NAME.replace("template", i18nSuffix);
         Files.write(Paths.get(config.getAnalysisReportPath(), reportName), html.getBytes(StandardCharsets.UTF_8));
     }
 
